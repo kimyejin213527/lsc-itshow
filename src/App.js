69a73ac27b771bd25bafc88fa266840bbd47464a
hyennin <chm050202@gmail.com>
2023-02-15 21:59:13 +0900

    navbar ui

--- src/App.js
+++ src/App.js
@@ -1,25 +1,135 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-import Home from './components/pages/MainPage';
-import Test from './components/pages/testPages/Condition';
-import Info from './components/pages/infoPages/FitnessInfo';
-import MyPage from './components/pages/MyPage';  
-import Nav from './components/ui/Nav';
+import React, { useState } from "react";
+import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faUser, faTimes, faHeart } from "@fortawesome/free-solid-svg-icons";
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <Nav />
-		{/* <Routes>
-			<Route exact path="/" component={<Home/>}></Route>
-			<Route path="/test" component={<Test/>}></Route>
-			<Route path="/info" component={<Info/>}></Route>
-			<Route path="/mypage" component={<MyPage/>}></Route>
-		</Routes> */}
-      </div>
-    )
+const Header = styled.div`
+  max-width: 1280px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  color: white;
+  background-color: #558B2F;
+
+  .logo {
+    margin: 0 1rem;
+    font-size: 2rem;
   }
+
+  .header__menulist {
+    list-style: none;
+    display: flex;
+  }
+
+  .header__left {
+    display: flex;
+  }
+
+  .header__right {
+    list-style: none;
+    display: flex;
+  }
+
+  .header__right div {
+    margin: 0 1rem;
+  }
+
+  li {
+    padding: 0 1rem;
+    cursor: pointer;
+  }
+
+  .toggle {
+    display: none;
+    font-size: 1.5rem;
+    padding: 1rem 1rem;
+  }
+
+  .user {
+    display: none;
+    font-size: 1.5rem;
+    padding: 1rem 1rem;
+  }
+
+  @media screen and (max-width: 768px) {
+    flex-wrap: wrap;
+
+    .header__right {
+      display: ${(props) => (props.userToggled ? "flex" : "none")};
+      flex-direction: column;
+      width: 100%;
+      background-color: black;
+    }
+
+    .header__menulist {
+      display: ${(props) => (props.isToggled ? "flex" : "none")};
+      flex-direction: column;
+      width: 100%;
+      background-color: black;
+    }
+
+    .header__menulist li,
+    .header__right li {
+      margin: 1rem 0;
+      padding: 0;
+    }
+
+    .toggle {
+      display: block;
+    }
+
+    .user {
+      display: block;
+    }
+  }
+`;
+
+function App() {
+  const [isToggled, setIsToggled] = useState(false);
+  const [userToggled, setUserToggled] = useState(false);
+
+  return (
+    <Header isToggled={isToggled} userToggled={userToggled}>
+      {/* 햄버거 버튼(bar) */}
+      <div
+        className="toggle"
+        onClick={() => {
+          setIsToggled(!isToggled);
+        }}
+      >
+        <FontAwesomeIcon icon={!isToggled ? faBars : faTimes} />
+      </div>
+
+      {/* 로고 */}
+      <div className="logo">
+        <FontAwesomeIcon icon={faHeart} />
+      </div>
+
+      {/* User 버튼 */}
+      <div
+        className="user"
+        onClick={() => {
+          setUserToggled(!userToggled);
+        }}
+      >
+        <FontAwesomeIcon icon={!userToggled ? faUser : faTimes} />
+      </div>
+
+      {/* 메뉴 리스트 */}
+      <ul className="header__menulist">
+        <li>테스트</li>
+        <li>정보</li>
+        <li>마이페이지</li>
+      </ul>
+
+      {/* User 메뉴 리스트 */}
+      <ul className="header__right">
+        <li>로그인</li>
+        <li>회원가입</li>
+      </ul>
+    </Header>
+  );
 }
 
 export default App;
