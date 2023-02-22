15a65a7459ae6baed3f04c9664692a6ead0056bb
hyennin <chm050202@gmail.com>
2023-02-22 21:54:29 +0900

    슬라이드 넘길 때마다 dot 색 변경

--- src/components/ui/Slider.jsx
+++ src/components/ui/Slider.jsx
@@ -4,57 +4,58 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 
 const Slider = () => {
-  const contents = useRef([
-    {src: "http://sanriokorea.co.kr/wp-content/themes/sanrio/images/kuromi.png"}, 
-    {src: "http://sanriokorea.co.kr/wp-content/themes/sanrio/images/n_fairilu_v1.png"}, 
-    {src: "http://sanriokorea.co.kr/wp-content/themes/sanrio/images/m_cinnamo.png"}]);
-  
-  const [current, setCurrent] = useState(0);
-  const [style, setStyle] = useState({
-    marginLeft: `-${current}00%`
-  });
-  const contentSize = useRef(contents.current.length);
-
-  const moveSlide = (i) => {
-    let nextIndex = current + i;
+    const contents = useRef([
+        {src: "http://sanriokorea.co.kr/wp-content/themes/sanrio/images/kuromi.png"}, 
+        {src: "http://sanriokorea.co.kr/wp-content/themes/sanrio/images/n_fairilu_v1.png"}, 
+        {src: "http://sanriokorea.co.kr/wp-content/themes/sanrio/images/m_cinnamo.png"}]);
     
-    if (nextIndex < 0) nextIndex = contentSize.current - 1;
-    else if (nextIndex >= contentSize.current) nextIndex = 0;
+    const [current, setCurrent] = useState(0);
+    const [style, setStyle] = useState({
+        marginLeft: `-${current}00%`
+    });
+    const contentSize = useRef(contents.current.length);
 
-    setCurrent(nextIndex);
-  };
+    const moveSlide = (i) => {
+        let nextIndex = current + i;
+        
+        if (nextIndex < 0) nextIndex = contentSize.current - 1;
+        else if (nextIndex >= contentSize.current) nextIndex = 0;
 
-  useEffect(() => {
-      setStyle({ marginLeft: `-${current}00%` });
-  }, [current]);
+        setCurrent(nextIndex);
+    };
+
+    useEffect(() => {
+        setStyle({ marginLeft: `-${current}00%` });
+    }, [current]);
   
-  return(
-    <Wrapper>
-        <Container>
-        <Slide>
-            <Button onClick={() => { moveSlide(-1); }}> <FontAwesomeIcon icon={faChevronLeft} /></Button>
-            <Window>
-            <ImgWrapper style={style}>
-                {contents.current.map((img, i) => (
-                <Img
+    return(
+        <Wrapper>
+            <Container>
+            <Slide>
+                <Button onClick={() => { moveSlide(-1); }}> <FontAwesomeIcon icon={faChevronLeft} /></Button>
+                <Window>
+                <ImgWrapper style={style}>
+                    {contents.current.map((img, i) => (
+                    <Img
+                        key={i}
+                        style={{ backgroundImage: `url(${img.src})` }}
+                    ></Img>
+                    ))}
+                </ImgWrapper>
+                </Window>
+                <Button onClick={() => { moveSlide(1); }}> <FontAwesomeIcon icon={faChevronRight} /></Button>
+            </Slide>
+            <Position>
+                {contents.current.map((x, i) => (
+                <Dot
                     key={i}
-                    style={{ backgroundImage: `url(${img.src})` }}
-                ></Img>
+                    style={{ background: `${(i === current ? 'grey' : 'lightgrey')}` }}
+                    ></Dot>
                 ))}
-            </ImgWrapper>
-            </Window>
-            <Button onClick={() => { moveSlide(1); }}> <FontAwesomeIcon icon={faChevronRight} /></Button>
-        </Slide>
-        <Position>
-            {contents.current.map((x, i) => (
-            <Dot
-                key={i}
-                ></Dot>
-            ))}
-        </Position>
-        </Container>
-    </Wrapper>
-  );
+            </Position>
+            </Container>
+        </Wrapper>
+    );
 };
 
 const Wrapper = styled.div`
@@ -113,7 +114,6 @@ const Position = styled.div`
 `;
 
 const Dot = styled.div`
-    background: grey;
     border-radius: 100%;
     height: 10px;
     width: 10px;
