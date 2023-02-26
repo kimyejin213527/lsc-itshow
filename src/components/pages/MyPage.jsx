19acf39c570cd21cefbe7d7a2c6311fe16847a50
yejin <yejin213527@gmail.com>
2023-02-26 22:42:09 +0900

    mypage start

--- src/components/pages/MyPage.jsx
+++ src/components/pages/MyPage.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
+import img from '../../img/mypage.png';
+import styled from 'styled-components';
+
 
 function MyPage(props) {
     return (
         <>
-            <h1>마이 페이지</h1>
+
+            <Img src={img} />
+            <p>아이디 : <br /> 이름 : <br /> 나이 : <br /> 성별 : <br /> 이메일 : </p>
+            <Img />
+
         </>
     )
 }
+
+
+const Img = styled.img
+    `
+width: 200px;
+height: 200px;
+`
+
+
 
 export default MyPage;
